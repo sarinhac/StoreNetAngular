0614f4be2895224700153fde51717ba3510bcc8b
sarinhac <sarahangelica.c@gmail.com>
2022-02-14 15:38:33 +0000

    InitialProject

---
+++ Curso.NetAngular/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Curso.NetAngular.Data;
+using Curso.NetAngular.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Curso.NetAngular.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // using "api/" is optional but it's conventional to do it
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int? id)
+        {
+            if(id is null)
+                return BadRequest();
+
+            Product product = await _context.Products.FindAsync(id);
+            
+            if (product is null)
+                return NotFound();
+            else
+                return Ok(product);
+        }
+    }
+}
---
+++ Curso.NetAngular/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Curso.NetAngular.Entities;
+
+namespace Curso.NetAngular.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
